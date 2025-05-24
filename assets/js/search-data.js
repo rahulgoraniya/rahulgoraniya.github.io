aec0a91cf2645b8e54047288760e8cbfdfe1a871
rahulgoraniya <163146469+rahulgoraniya@users.noreply.github.com>
2025-05-24 20:50:14 +0000

    Deploying to gh-pages from @ rahulgoraniya/rahulgoraniya.github.io@d6c0637a7ce5dbcd67f77f0aeaae057001b0b236 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -104,7 +104,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%61%68%75%6C%67%6F%72%61%6E%69%79%61%37%36%37%36@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-x',
