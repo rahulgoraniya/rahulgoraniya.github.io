08d0185feff90d0f3060f8ce238080fa10b9c601
rahulgoraniya <163146469+rahulgoraniya@users.noreply.github.com>
2026-05-08 15:51:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.add("light-mode");
     toggleButton.textContent = "☾";
 } else {
-  toggleButton.textContent = "☀️";
+  toggleButton.textContent = "☼";
 }
 
   toggleButton.addEventListener("click", () => {
