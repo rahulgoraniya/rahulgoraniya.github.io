5e7caa4893beb56a75ba1078acbf6b40fe8ce617
rahulgoraniya <163146469+rahulgoraniya@users.noreply.github.com>
2026-06-04 12:55:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,9 +57,3 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-const menuToggle = document.getElementById("menu-toggle");
-const mobileNav = document.getElementById("mobile-nav");
-
-menuToggle.addEventListener("click", () => {
-  mobileNav.classList.toggle("show");
-});
